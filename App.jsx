6b4e3fc3bea57c06f8a52bda363672506179d3e0
Rajeev Tapadia <rtapadia730@gmail.com>
2024-04-11 13:44:49 +0530

    dashboard screen

--- App.jsx
+++ App.jsx
@@ -1,26 +1,47 @@
-import React, {useEffect, useState} from 'react';
-import {SafeAreaView, Text, useColorScheme, View} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {SafeAreaView, Text, View, useColorScheme} from 'react-native';
+import {PaperProvider, DefaultTheme} from 'react-native-paper';
 import {connectToDatabase, createTables} from './src/database/database';
+import FuelEntryCard from './src/components/FuelEntryCard';
+import NavBar from './src/components/NavBar';
+import MeterReadingCard from './src/components/MeterReadingCard';
+import global from './src/styles/global';
 
 function App() {
   const [db, setDb] = useState(null);
-  const isDarkMode = useColorScheme() === 'dark';
+  const darkTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: 'rgb(220, 184, 255)',
+      accent: '#ffc107',
+      background: '#1F1F1F',
+      surface: '#121212',
+      text: '#fff',
+    },
+    roundness: 4, // Adjust corner radius if needed
+  };
 
-  useEffect(() => {
-    async function initDb() {
-      const connection = await connectToDatabase();
-      setDb(connection);
-      createTables(connection);
-    }
-    initDb();
+  const connectDB = useCallback(async () => {
+    const connection = await connectToDatabase();
+    setDb(connection);
+    createTables(connection);
   }, []);
 
+  useEffect(() => {
+    connectDB();
+  }, [connectDB]);
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>gg lol</Text>
-      </View>
-    </SafeAreaView>
+    <PaperProvider theme={darkTheme}>
+      <SafeAreaView>
+        <View style={global.window}>
+          <NavBar title="Dashboard" />
+          <FuelEntryCard />
+          <MeterReadingCard />
+        </View>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
---
+++ src/styles/global.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  window: {
+    backgroundColor: '#1F1F1F',
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    color: 'white',
+  },
+});
+
+export default styles;
