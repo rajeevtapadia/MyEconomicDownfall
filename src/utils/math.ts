ac3d0f5deef7ed83e8dde27de393249a27848d3b
Rajeev Tapadia <rtapadia730@gmail.com>
2024-04-14 23:07:19 +0530

    calculate latest avg

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -34,3 +34,54 @@ export async function calcOverallAvg(db: SQLiteDatabase) {
     return NaN;
   }
 }
+
+// calculates the most recent fill's avg
+export async function calcLatestFillAvg(db: SQLiteDatabase): Promise<number> {
+  try {
+    const [readingQuery] = await db.executeSql(`
+      SELECT * FROM Reading ORDER BY date DESC;
+    `);
+
+    const readingData = readingQuery.rows.raw();
+
+    let lastReading = readingData[0];
+    let secondLastR = readingData[1];
+
+    const lastFill = await calcFillTill(db, secondLastR.date, lastReading.date);
+
+    const latestFillAvg =
+      (lastReading.meterReading - secondLastR.meterReading) / lastFill;
+
+    return latestFillAvg;
+  } catch (error) {
+    console.log(error);
+    return NaN;
+  }
+}
+
+// calculates fuel consumption between a range of date
+export async function calcFillTill(
+  db: SQLiteDatabase,
+  start: string,
+  end: string,
+): Promise<number> {
+  try {
+    const [fuelQuery] = await db.executeSql(`
+      SELECT * FROM FuelQuantity ORDER BY date DESC;
+    `);
+
+    const fuelData = fuelQuery.rows.raw();
+    let sum = 0;
+    for (let i = 0; i < fuelData.length; i++) {
+      if (fuelData[i].date <= end && fuelData[i].date >= start) {
+        sum += fuelData[i].quantity;
+      }
+    }
+
+    console.log({sum, start, end});
+    return sum;
+  } catch (e) {
+    console.error(e);
+    return NaN;
+  }
+}
